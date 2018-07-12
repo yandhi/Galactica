53175407cf988f3395a07165fff7b348ff550879
Kix <ellsworthjackson@gmail.com>
2018-07-12 14:15:10 -0400

    Fixed CancelableEvents

--- src/main/java/me/kix/galactica/event/cancelable/CancelableEvent.java
+++ src/main/java/me/kix/galactica/event/cancelable/CancelableEvent.java
@@ -1,10 +1,12 @@
 package me.kix.galactica.event.cancelable;
 
+import me.kix.galactica.event.Event;
+
 /**
  * @author Kix
  * @since 7/8/2018
  */
-public class CancelableEvent implements Cancelable {
+public class CancelableEvent extends Event implements Cancelable {
 
 	private boolean canceled;
 
